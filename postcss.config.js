c26fbe88e9dfacf6718cb4e77d92259661906fd8
Eric Arwido Damanik <arwido.eric@gmail.com>
2025-04-07 16:47:08 +0700

    Add vercel-build script

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  };
-  
+// postcss.config.js
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
